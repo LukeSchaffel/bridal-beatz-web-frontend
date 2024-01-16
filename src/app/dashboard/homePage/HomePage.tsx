5ab4208f49fe41c2d2ec79661c3a9feb19578771
LukeSchaffel <lukeschaffel@gmail.com>
2024-01-16 11:11:27 -0500

    search for vendors on homepage

--- src/app/dashboard/homePage/HomePage.tsx
+++ src/app/dashboard/homePage/HomePage.tsx
@@ -1,10 +1,11 @@
-import { Typography, Skeleton } from 'antd'
+import { Typography, Skeleton, Segmented, Select, Spin } from 'antd'
 import { useTypedSelector, useAppDispatch } from '../../hooks'
 
 import { getAccounts } from '../../../features/accounts/accounts.slice'
-import styles from './_home_page.module.scss'
 import { useEffect, useState } from 'react'
 import ProfileCard from './profileCard/ProfileCard'
+import styles from './_home_page.module.scss'
+import { states } from '../../../utils/misc'
 
 const { Title } = Typography
 
@@ -13,6 +14,7 @@ type TQueryState = {
 	search: string
 	client_type: Account['client_type']
 	vendor_type: Account['vendor_type']
+	state: string
 }
 
 const HomePage = () => {
@@ -24,6 +26,7 @@ const HomePage = () => {
 		search: '',
 		client_type: undefined,
 		vendor_type: undefined,
+		state: '',
 	}
 	const [query, setQuery] = useState({ ...initialQueryState })
 
@@ -41,15 +44,71 @@ const HomePage = () => {
 
 	useEffect(() => {
 		handleGetAccounts()
-	}, [])
+	}, [query])
+
+	const onSegmentedChange = (value) => {
+		setQuery({
+			...query,
+			[account.type === 'client' ? 'vendor_type' : 'client_type']: value,
+		})
+	}
+
+	const vendorSegmented = (
+		<Segmented
+			options={[
+				{
+					label: 'Any client',
+					value: '',
+				},
+				{
+					label: 'Bride',
+					value: 'bride',
+				},
+				{ label: 'Groom', value: 'groom' },
+				{ label: 'Venue', value: 'venue' },
+			]}
+			onChange={onSegmentedChange}
+		/>
+	)
+	const clientSegmented = (
+		<Segmented
+			options={[
+				{
+					label: 'Any vendor',
+					value: '',
+				},
+				{
+					label: 'Band',
+					value: 'band',
+				},
+				{ label: 'DJ', value: 'dj' },
+				{ label: 'Musician', value: 'musician' },
+			]}
+			onChange={onSegmentedChange}
+		/>
+	)
+
+	const onSelectChange = (value) => {
+		setQuery({
+			...query,
+			state: value,
+		})
+	}
 
 	return (
 		<>
 			<div className={styles.main}>
 				<Title>Welcome {account.first_name}</Title>
+				<div className={styles.header}>
+					Im looking for a {account.type === 'vendor' && vendorSegmented} {account.type === 'client' && clientSegmented}{' '}
+					in: <Select allowClear onChange={onSelectChange} style={{ width: 100 }} options={states} />
+				</div>
+				<br />
 				<div>
 					{status === 'pending' ? (
-						<Skeleton active />
+						<div className={styles.spinContainer}>
+							<Spin size="large" />
+						</div>
 					) : (
 						<div className={styles.accountsContainer}>
 							{accounts.map((account: Account) => (
